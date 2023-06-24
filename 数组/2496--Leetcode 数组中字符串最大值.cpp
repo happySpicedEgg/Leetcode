2565e9e305c77deef4250d7348b66eba0e09da91
Egg <2856594865@qq.com>
2023-06-24 20:25:52 +0800

    6月24日两数之和提交

---
+++ 数组/2496--Leetcode 数组中字符串最大值.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+#include<vector>
+#include<string>
+using namespace std;
+    
+
+class Solution {
+public:
+    int maximumValue(vector<string>& strs) {
+        int res = 0;
+        for (auto& s : strs) {
+            bool isDigits = true;
+            for (char& c : s) {
+                isDigits &= isdigit(c);
+            }
+            res = max(res, isDigits ? stoi(s) : (int)s.size());
+        }
+        return res;
+    }
+};
+
