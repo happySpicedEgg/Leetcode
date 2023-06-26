0290a4b78c717169c3b7c10f784087922a8e8da5
Egg <2856594865@qq.com>
2023-06-27 00:50:27 +0800

    提交Leetcode 二分查找之搜索插入位置

---
+++ 数组/35--Leetcode 二分查找之搜索插入位置.cpp
@@ -0,0 +1,21 @@
+﻿class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right) {
+            int middle = (left + right) / 2;
+            if (nums[middle] > target) {
+                right = middle - 1;
+            }
+            else if (nums[middle] < target) {
+                left = middle + 1;
+            }
+            else {
+                return middle;
+            }
+        }
+        return right + 1;
+    }
+};
+//一定要搞清楚二分查找后left和right之间的关系，如果找到值left和right是什么，如果没找到值那么right和left又等于什么
